b58a5b25ed46fbd51466bb695e55f91a69e9a7a2
KuzuSy <72185113+KuzuSy@users.noreply.github.com>
2020-12-03 09:02:41 +0800

    Add files via upload

---
+++ HTML/js/js.js
@@ -0,0 +1,115 @@
+﻿//somev
+var body=document.body;
+var bgImgId=Math.round(Math.random()*100)%7;
+
+//json
+for(var i=0 in json)
+{
+    var text='<h3><span class="headline '+json[i].type+'"><font>●</font>'
+        +json[i].name+'</span></h3>';
+    text=text+'\n<div style="display:none;">';
+    text=text+'\n<p class="producer basicInf"><b>动画制作</b>:<i>'
+        +json[i].producer+'</i></p>';
+    text=text+'\n<p><a href="'+json[i].site+
+        '" class="site basicInf"><b>官网ClickHere</b></a></p>';
+    for(var j=0 in json[i].charactor)
+        text=text+"\n<a>"+json[i].charactor[j].name+"</a><br>";
+    text=text+'\n<p class="review"><b>'+json[i].review+'</b></p>';
+    $("#"+json[i].time).append(text);
+}
+
+//fucktion
+function randomColor()
+{
+    var colArr = new Array('7','8','9','a','b','c','d','e','f');
+    var color = '';
+    for(var i = 0;i<6;i++)
+    {
+        var random = Math.floor(Math.random()*9);
+        color+=colArr[random];
+    }
+    return "#"+color;
+}
+function changeBackground()
+{
+    var img_url='animeSource/bg'+bgImgId+'.jpg';
+    body.style.backgroundImage='url("'+img_url+'")';
+    var img=document.createElement("img");img.src=img_url;
+    img.onload=function()
+    {
+        if(window.innerWidth/window.innerHeight>img.width / img.height)
+            body.style.animation="backgroundW 3s";
+        else
+            body.style.animation="backgroundH 3s";
+    };
+    bgImgId=(bgImgId+1)%7;
+}
+
+//jq
+var a_idx = 0;
+jQuery(document).ready(function($)
+{
+    onkeydown=function(event) {
+        if(event.which==13)changeBackground();
+    }
+    $("body").click(function(e)
+    {
+        var a = new Array("loli控","NTR", "死宅", "PRPR", "人活着就是为了樱岛麻衣", "HENTAI","妹控", "GALGAME", "素股","二次元");
+        var $i = $("<span/>").text(a[a_idx]);
+        a_idx = (a_idx + 1)% a.length;
+        var x = e.pageX, y = e.pageY,color=randomColor;
+        $i.css(
+        {
+            "z-index" : 99999999,
+            "top" : y - 20,
+            "left" : x,
+            "position" : "absolute",
+            "font-weight" : "bold",
+            "color" : color
+        });
+        $("body").append($i);
+        $i.animate(
+        {
+            "top" : y - 180,
+            "opacity" : 0
+        }, 1500, function()
+        {
+            $i.remove();
+        });
+    });
+    $("#Filo").hover(function(e)
+    {
+        $("#charaPic").css({
+            "display":"block",
+            "animation":"picAppear 2s forwards",
+        })
+    },function(e)
+    {
+        $("#charaPic").css({
+            "animation":"picDisappear 2s 0.3s forwards",
+        })
+    });
+});
+
+//body
+var p=document.getElementsByClassName("headline"),br=document.createElement("br");
+changeBackground();
+for(i=0;i<p.length;i++)
+{
+    p[i].onclick = function()
+    {
+        var bro=this.parentElement.nextElementSibling;
+        if(bro.style.display=="none")
+	        bro.style.display="block";
+        else bro.style.display="none";
+    };
+    p[i].style.cursor="default";
+}
+allclose.onclick = function()
+{
+    var p=document.getElementsByClassName("headline");
+    for(i=0;i<p.length;i++)
+    p[i].parentElement.nextElementSibling.style.display="none";
+};
+anime_total.innerHTML="已经看完"+document.getElementsByClassName("anime").length+"部番剧";
+movie_total.innerHTML="已经看完"+document.getElementsByClassName("movie").length+"部电影/剧场版";
---
+++ HTML/json/json.js
@@ -0,0 +1,664 @@
+﻿var json=
+[
+	{
+		"time":"2020Autumn",
+		"name":"龙与魔女 | BURN THE WITCH",
+		"producer":"team Yamahitsuji | Studio Colorido",
+		"site":"https://burn-the-witch-anime.com/",
+		"charactor":
+		[
+			{"name":"妮内·斯邦库尔 | Ninny Spangcole"},
+			{"name":"新桥诺艾尔 | 新橋のえる"},
+			{"name":"巴尔戈·帕克斯 | ??"},
+		],
+		"review":'带师出品，Tonikaku就是热血沸腾，所以三集太少啦!'
+	},
+	{
+		"time":"2020Summer",
+		"name":"大神同学想要被吃掉 | オオカミさんは食べられたい",
+		"producer":"ピークハント",
+		"site":"https://lp.wwwave.jp/comicgp/okamisan.php",
+		"charactor":
+		[
+			{"name":"赤頭辰美 | AKAZU TATSUMI"},
+			{"name":"大神ひな子 | OKAMI HINAKO"},
+		],
+		"review":'懂都懂'
+	},
+	{
+		"time":"2020Summer",
+		"name":"DECA-DENCE | デカダンス",
+		"producer":"NUT",
+		"site":"http://decadence-anime.com/",
+		"charactor":
+		[
+			{"name":"枣 | ナツメ"},
+			{"name":"镝木 | カブラギ"},
+		],
+		"review":'Natsume好可爱（馋身子）'
+	},
+	{
+		"time":"2020Summer",
+		"name":"Re：从零开始的异世界生活Ⅱ(前半) | Re：ゼロから始める異世界生活Ⅱ",
+		"producer":"WHITE FOX",
+		"site":"http://re-zero-anime.jp/",
+		"charactor":
+		[
+			{"name":"菜月昴 | ナツキ・スバル"},
+			{"name":"爱蜜莉雅 | エミリア"},
+			{"name":"艾姬多娜 | エキドナ"},
+		],
+		"review":'7月观感最佳，486真男人，不愧私の憧憬の男'
+	},
+	{
+		"time":"2020Summer",
+		"name":"高校之神 | THE GOD OF HIGH SCHOOL",
+		"producer":"MAPPA",
+		"site":"http://goh-anime.com/",
+		"charactor":
+		[
+			{"name":"陈毛利 | ジン・モリ"},
+			{"name":"诸葛泽 | ？？"},
+		],
+		"review":'打斗是真酷炫。。前期剧情小崩'
+	},
+	{
+		"time":"2020Summer",
+		"name":"魔王学院的不适任者～史上最强的魔王始祖，转生就读子孙们的学校～ | 魔王学院の不适合者～史上最强の魔王の始祖、转生して子孙たちの学校へ通う～",
+		"producer":"SILVER LINK.",
+		"site":"https://maohgakuin.com/",
+		"charactor":
+		[
+			{"name":"ANOS VOLDIGOAD | アノス・ヴォルディゴード"},
+			{"name":"米夏·涅库洛 | ミーシャ・ネクロン"},
+			{"name":"莎夏·涅库洛 | サーシャ・ネクロン"},
+			{"name":"雷伊·格兰兹多利 | ？？"},
+		],
+		"review":'ANIPEX。。'
+	},
+	{
+		"time":"2020Summer",
+		"name":"租借女友 | 彼女、お借りします",
+		"producer":"TMS Entertainment",
+		"site":"https://kanokari-official.com/",
+		"charactor":
+		[
+			{"name":"木ノ下和也 | Kinoshita Kazuya"},
+			{"name":"一ノ瀬千鶴 | Ichinose Chizuru"},
+			{"name":"七海麻美 | Nanami Mami"},
+			{"name":"更科瑠夏 | Sarashina Ruka"},
+			{"name":"桜沢墨 | Sakurasawa Sumi"},
+		],
+		"review":'总之就是非常修罗场（总之男主就是非常废物）'
+	},
+	{
+		"time":"2020Summer",
+		"name":"宇崎学妹想要玩！ | 宇崎ちゃんは遊びたい！",
+		"producer":"ENGI",
+		"site":"https://uzakichan.com/",
+		"charactor":
+		[
+			{"name":"宇崎花 | Uzaki Hana"},
+			{"name":"桜井真一 | Sakurai Shinichi"},
+		],
+		"review":'大大大（指眼睛）大大大（<a style="color:yellow">??</a>）日常向看着挺好'
+	},
+	{
+		"time":"2020Summer",
+		"name":"刀剑神域 Alicization War of Underworld -THE LAST SEASON- | ソードアート・オンライン",
+		"producer":"A-1 Pictures",
+		"site":"https://sao-alicization.net/",
+		"charactor":
+		[
+			{"name":"桐谷和人 | Kirito"},
+			{"name":"Alice | アリス・シンセシス・サーティ"},
+		],
+		"review":'SHIT!NTR?小野学吃屎'
+	},
+	{
+		"time":"2020Summer",
+		"name":"彼得·格里尔的贤者时间 | ピーター・グリルと賢者の時間",
+		"producer":"WolfsBane",
+		"site":"http://petergrill-anime.jp/",
+		"charactor":
+		[
+			{"name":"彼得·格里尔 | ピーター・グリル"},
+			{"name":"露薇莉亚·桑克图斯 | ルヴェリア・サンクトゥス"},
+			{"name":"莉莎·阿尔帕卡斯 | リサ・アルパカス"},
+			{"name":"美美·阿尔帕卡斯 | ミミ・アルパカス"},
+			{"name":"薇绀·埃德里尔 | ビーガン・エルドリエル"},
+			{"name":"皮格莉特·潘塞特 | ピグリット・パンチェッタ"},
+		],
+		"review":'美少女NTR，好!'
+	},
+	{
+		"time":"2020Spring",
+		"name":"辉夜大小姐想让我告白<s>～天才们的恋爱头脑战～</s> | かぐや様は告らせたい<s>～天才たちの恋爱头脳戦～</s>",
+		"producer":"A-1 Pictures",
+		"site":"http://kaguya.love/",
+		"charactor":
+		[
+			{"name":"四宮かぐや | Shinomiya Kaguya"},
+			{"name":"白銀御行 | Shirogane Miyuki"},
+			{"name":"藤原千花 | Fujiwara Chika"},
+			{"name":"石上優 | Ishigami Yu"},
+			{"name":"伊井野ミコ | Iino Miko"},
+			{"name":"白銀圭 | Shirogane Kei"},
+			{"name":"早坂愛 | Hayasaka Ai"},
+		],
+		"review":'新增了好多老婆!（不是）Aniplex画风好稳'
+	},
+	{
+		"time":"2020Spring",
+		"name":"公主连结Re:Dive | Princess Connect! Re:Dive",
+		"producer":"CygamesPictures",
+		"site":"https://anime.priconne-redive.jp/",
+		"charactor":
+		[
+			{"name":"佩可莉姆 | ペコリーヌ"},
+			{"name":"凯露 | キャル"},
+			{"name":"可可萝 | コッコロ"},
+			{"name":"佑树 | ユウキ"},
+		],
+		"review":'马上就井克总，马上就井克总'
+	},
+	{
+		"time":"2020Spring",
+		"name":"神之塔 -Tower of God-",
+		"producer":"Telecom Animation Film",
+		"site":"https://tog-anime.com/",
+		"charactor":
+		[
+			{"name":"第二十五夜"},
+			{"name":"昆·阿圭罗·阿克尼斯"},
+			{"name":"蕾哈尔"},
+		],
+		"review":'小信？？阿哲。。一改韩漫印象（）真香'
+	},
+	{
+		"time":"2020Spring",
+		"name":"转生成为了只有乙女游戏破灭Flag的邪恶大小姐 | 乙女ゲームの破滅フラグしかない悪役令嬢に転生してしまった…",
+		"producer":"SILVER LINK.",
+		"site":"https://hamehura-anime.com/",
+		"charactor":
+		[
+			{"name":"卡塔莉娜·克拉艾斯 | カタリナ・クラエス"},
+			{"name":"索菲亚·亚斯卡鲁特 | ソフィア?アスカルト"},
+			{"name":"玛丽亚·坎贝尔 | マリア?キャンベル"},
+		],
+		"review":'4月份逆后宫真奇妙，4月份逆后宫真奇妙'
+	},
+	{
+		"time":"2020Spring",
+		"name":"八男?别闹了! | 八男って、それはないでしょう！",
+		"producer":"SHIN-EI动画 | SynergySP",
+		"site":"http://hachinan-anime.com/",
+		"charactor":
+		[
+			{"name":"威德林·冯·班诺·鲍麦斯特 | ヴェンデリン"},
+			{"name":"艾莉丝·卡特琳娜·冯·霍恩海姆 | エリーゼ?カタリーナ?フォン?ホーエンハイム"},
+			{"name":"伊娜·苏珊·希伦布兰德 | イーナ?ズザネ?ヒレンブラント"},
+			{"name":"露易丝·尤兰妲·奥蕾莉亚·欧佛维克 | ルイーゼ?ヨランデ?アウレリア?オーフェルヴェーク"},
+			{"name":"维尔玛 | ??"},
+		],
+		"review":'（钱真少）。老铁们，只有你们想不到的，没有我老八做不到的啊，还是那句话，今天我老八挑战一把开后宫，兄弟们，奥利给，开就完了！（来自网评，妙啊）'
+	},
+	{
+		"time":"2020Spring",
+		"name":"天晴烂漫！ | 天晴爛漫！",
+		"producer":"P.A.WORKS",
+		"site":"http://appareranman.com/",
+		"charactor":
+		[
+			{"name":"空乃天晴 | ?? Appare"},
+			{"name":"一色小雨"},
+			{"name":"景·夏莲"},
+			{"name":"索菲娅·泰勒"},
+			{"name":"迪伦·G·奥尔丁"},
+			{"name":"吉尔·T·西格（理查德·利斯曼）"},
+		],
+		"review":'不要热血少年锤反派啊'
+	},
+	{
+		"time":"2020Winter",
+		"name":"虚构推理 | 虚構推理",
+		"producer":"Brain's Base",
+		"site":"https://kyokousuiri.jp/",
+		"charactor":
+		[
+			{"name":"岩永琴子 | Iwanaga Kotoko"},
+			{"name":"樱川九郎 | Sakuragawa Kurou"},
+		],
+		"review":'色批女主爱了爱了，萌点!'
+	},
+	{
+		"time":"2020Winter",
+		"name":"异种族风俗娘评鉴指南 | 異種族レビュアーズ",
+		"producer":"Passione",
+		"site":"https://isyuzoku.com/",
+		"charactor":
+		[
+			{"name":"史坦克 | Stunk"},
+			{"name":"可利姆维儿 | Crimvael"},
+			{"name":"梅多莉 | Meidri"},
+		],
+		"review":'世纪最强表番，我愿称之2020封神！观感爆了，神仙啊，这就是ERO的SEIKAI吗(除了那奶奶，淦！)「名誉の戦死」、「もはや地上波に楽園は無い」'
+	},
+	{
+		"time":"2020Winter",
+		"name":"异度侵入 | ID:INVADED",
+		"producer":"NAZ",
+		"site":"https://id-invaded-anime.com/",
+		"charactor":
+		[
+			{"name":"鳴瓢秋人 | なりひさごあきひと | 酒井戸 | さかいど"},
+			{"name":"佳爱琉 | Kaeru | 飛鳥井木記 | あすかいきき"},
+			{"name":"本堂町小春 | ほんどうまちこはる | 聖井戸御代 | ひじりいどみよ"},
+			{"name":"富久田保津 | ふくだたもつ | 穴井戸 | あないど"},
+		],
+		"review":'吹爆吹爆，烧脑番一向的好爆了，BUTTERFLY真好听，从此粉津叔'
+	},
+	{
+		"time":"2020Winter",
+		"name":"地缚少年花子君 | 地縛少年花子くん",
+		"producer":"Lerche",
+		"site":"https://dorohedoro.net/",
+		"charactor":
+		[
+			{"name":"花子君 | 柚木普 | Yugi Amane"},
+			{"name":"八尋寧々 | Yahiro Nene"},
+			{"name":"赤根葵 | Akane Aoi"},
+		],
+		"review":'画风清新，绪方姐真的强（狛枝声线），老夫有幸观摩这少女漫画一般的动画'
+	},
+	{
+		"time":"2020Winter",
+		"name":"成群结伴！西顿学园 | 群れなせ！シートン学園",
+		"producer":"Studio五组",
+		"site":"https://anime-seton.jp/",
+		"charactor":
+		[
+			{"name":"间样人"},
+			{"name":"大狼兰华"},
+			{"name":"牝野瞳"},
+			{"name":"兽生美尤妃"},
+		],
+		"review":'科普番，太奇妙、、、兰华萝莉（prprpr不是）kawaiii'
+	},
+	{
+		"time":"2020Winter",
+		"name":"怕痛的我，把防御力点满就对了 | 痛いのは嫌なので防御力に極振りしたいと思います。",
+		"producer":"SILVER LINK.",
+		"site":"https://bofuri.jp/",
+		"charactor":
+		[
+			{"name":"梅普露 | Honjo Kaede | 本条楓"},
+			{"name":"莎莉 | 白峯理沙  | しらみねりさ"},
+			{"name":"奏 | カナデ"},
+		],
+		"review":'盾之勇者成名录是一部非常好看的番，盾之勇者防御力非常高（混乱）'
+	},
+	{
+		"time":"2019Autumn",
+		"name":"刀剑神域 Alicization War of Underworld | ソードアート・オンライン",
+		"producer":"A-1 Pictures",
+		"site":"https://sao-alicization.net/",
+		"charactor":
+		[
+			{"name":"桐谷和人 | Kirito"},
+			{"name":"Alice | アリス・シンセシス・サーティ"},
+			{"name":"UGO | ユージオ"},
+		],
+		"review":'战斗特效拉满，ALICE老婆（GET）★'
+	},
+	{
+		"time":"2019Autumn",
+		"name":"齐木楠雄的灾难 再始动篇 | 斉木楠雄のΨ難",
+		"producer":"J.C.STAFF",
+		"site":"https://www.saikikusuo.com/",
+		"charactor":
+		[
+			{"name":"斉木楠雄 | Saiki Kusuo"},
+			{"name":"燃堂力 | Nendou Riki"},
+			{"name":"海藤瞬 | Kaidou Shyun"},
+			{"name":"照橋心美 | Teruhashi Kokomi"},
+		],
+		"review":'看着非常有日本漫画的感觉ΨΨΨΨΨΨΨ'
+	},
+	{
+		"time":"2019Autumn",
+		"name":"慎重勇者～这个勇者明明超强却过分慎重～ | 慎重勇者～この勇者が俺TUEEEくせに慎重すぎる～",
+		"producer":"WHITE FOX",
+		"site":"http://shincho-yusha.jp/",
+		"charactor":
+		[
+			{"name":"竜宮院聖哉 | Ryuguin Seiya"},
+			{"name":"莉丝妲黛 | リスタルテ"},
+			{"name":"阿丽雅朵亚 | アリアドア"},
+			{"name":"雅黛涅拉 | アデネラ"},
+		],
+		"review":'原来：新晋阿库娅wwww，龙傲天爽啊。后来：剧情？？？泪目'
+	},
+	{
+		"time":"2019Autumn",
+		"name":"我，不是说了能力要平均值么 | 私、能力は平均値でって言ったよね！",
+		"producer":"project No.9",
+		"site":"https://noukin-anime.com/",
+		"charactor":
+		[
+			{"name":"阿黛尔·冯·阿斯卡姆 | 麦露 | マイル | 栗原海里"},
+			{"name":"烈娜 | レーナ"},
+		],
+		"review":'玩梗带师风傲天，百合向异世界下次还看'
+	},
+	{
+		"time":"2019Autumn",
+		"name":"入间同学入魔了 | 魔入りました！入間くん",
+		"producer":"BN Pictures",
+		"site":"http://www.nhk-character.com/chara/iruma/",
+		"charactor":
+		[
+			{"name":"铃木入间"},
+			{"name":"瓦拉克·克拉拉"},
+			{"name":"阿斯莫德·艾利斯"},
+		],
+		"review":'异世界沙雕*（这个动漫堪称“十月沙雕王--web)'
+	},
+	{
+		"time":"2019Summer",
+		"name":"Dr.STONE | 石纪元",
+		"producer":"TMS Entertainment",
+		"site":"https://dr-stone.jp/",
+		"charactor":
+		[
+			{"name":"石神千空 | いしがみせんくう"},
+			{"name":"琥珀 | コハク"},
+			{"name":"琉璃"},
+			{"name":"小川杠"},
+		],
+		"review":'科技番爱了爱了<s>486</s>千空发型好骚啊'
+	},
+	{
+		"time":"2019Summer",
+		"name":"在地下城寻求邂逅是否搞错了什么Ⅱ | ダンジョンに出会いを求めるのは間違っているだろうかⅡ",
+		"producer":"J.C.STAFF",
+		"site":"http://danmachi.com/danmachi2/",
+		"charactor":
+		[
+			{"name":"贝尔·克朗尼 | ベル?クラネル"},
+			{"name":"赫斯缇雅 | ヘスティア"},
+			{"name":"三条野·春姬 | サンジョウノ?春姫(はるひめ)"},
+			{"name":"艾丝·华伦斯坦 | アイズ?ヴァレンシュタイン"},
+		],
+		"review":'赫斯缇雅老婆啊啊啊啊（剑姬也是、春姬也是）我全都要！'
+	},
+	{
+		"time":"2019Summer",
+		"name":"天気の子 | Weathering With You",
+		"producer":"Comix Wave Films",
+		"site":"https://www.tenkinoko.com/",
+		"charactor":
+		[
+			{"name":"天野陽菜 | あまのひな"},
+			{"name":"森嶋帆高 | もりしまほだか"},
+		],
+		"review":'新海诚nb，画质太炸了。这种恋爱MOVIE看不厌呢（WRYY）'
+	},
+	{
+		"time":"2019Summer",
+		"name":"某科学的一方通行 | とある科学の一方通行",
+		"producer":"J.C.STAFF",
+		"site":"https://toaru-project.com/accelerator/",
+		"charactor":
+		[
+			{"name":"一方通行 | アクセラレータ | Accelerator | <s>鈴科百合子</s>"},
+			{"name":"埃斯特尔·罗森塔尔 | エステル?ローゼンタール"},
+			{"name":"最后之作 | 打ち止め | ラストオーダー"},
+			{"name":"菱形蛭魅 | ひしがたひるみ"},
+		],
+		"review":'“<span style="color:red;">我不管他是何方神圣，也无所谓其究竟是哪种组织，既然敢让我不爽，我定会亲手毁灭它！</span>”一方大爷yyds'
+	},
+	{
+		"time":"2019Summer",
+		"name":"中之人基因组【实况中】 | ナカノヒトゲノム【実況中】",
+		"producer":"SILVER LINK.",
+		"site":"http://www.nhg-anime.com/",
+		"charactor":
+		[
+			{"name":"入出 アカツキ(晓) | いりでアカツキ"},
+			{"name":"更屋敷 カリン(花凛) | さらやしきカリン"},
+			{"name":"鬼ヶ崎 カイコク(海国) | おにがさきカイコク"},
+			{"name":"駆堂 アンヤ(杏也) | くどうアンヤ"},
+			{"name":"忍雾石榴 ザクロ(忍霧) | おしぎりザクロ"},
+			{"name":"伊奈葉 ヒミコ(阳美子) | いなばヒミコ"},
+			{"name":"逢河 マキノ(牧野) | あいかわマキノ"},
+			{"name":"路々森 ユズ(柚子) | ろろもりユズ"},
+			{"name":"羊驼 | PAKA"},
+		],
+		"review":'DANGARONPA(并不是)，姑且定位轻日常番(雾)'
+	},
+	{
+		"time":"2019Summer",
+		"name":"平凡职业造就世界最强 | ありふれた職業で世界最強",
+		"producer":"asread | WHITE FOX",
+		"site":"https://arifureta.com/",
+		"charactor":
+		[
+			{"name":"南云始 | 南雲（なぐも） ハジメ"},
+			{"name":"月(ユエ) | アレーティア（娅蕾缇雅）"},
+			{"name":"希雅·郝里亚 | シア?ハウリア"},
+			{"name":"缇奥·库拉鲁斯 | ティオ?クラルス"},
+		],
+		"review":'黑化顶！美少女都超可爱（prprprprpr*∞）Tag：美食番（男主の名：屎）'
+	},
+	{
+		"time":"2019Summer",
+		"name":"重来吧，魔王大人！ | 魔王様、リトライ！",
+		"producer":"EKACHI EPILKA",
+		"site":"http://maousama-anime.com/",
+		"charactor":
+		[
+			{"name":"九内伯斗 | くない はくと"},
+			{"name":"亚克 | アク"},
+			{"name":"露娜·埃雷甘特 | ルナ?エレガント"},
+			{"name":"安洁尔·怀特 | エンジェル?ホワイト"},
+			{"name":"雪风"},
+		],
+		"review":'太穷了太穷了。。雪风（伪娘不多言pr）露娜koe suteki！'
+	},
+	{
+		"time":"2019Spring",
+		"name":"一拳超人Ⅱ | One-Punch Man",
+		"producer":"J.C.STAFF",
+		"site":"http://onepunchman-anime.net/",
+		"charactor":
+		[
+			{"name":"战栗的龙卷 | 战栗のタツマキ | 龙卷"},
+			{"name":"杰诺斯 | ジェノス"},
+			{"name":"埼玉 | サイタマ"},
+			{"name":"饿狼 | ガロウ"},
+		],
+		"review":'やばりロリ最高だ！！（竜巻）饿狼快杀光头啊！'
+	},
+	{
+		"time":"2019Spring",
+		"name":"进击的巨人Ⅲ Part.2 | 进撃の巨人",
+		"producer":"WIT STUDIO",
+		"site":"https://shingeki.tv/season3/",
+		"charactor":
+		[
+			{"name":"艾伦·耶格尔 | エレン?イェーガー"},
+			{"name":"三笠·阿克曼 | ミカサ?アッカーマン"},
+			{"name":"阿明·阿诺德 | アルミン?アルレルト"},
+			{"name":"吉克·耶格尔 | <s>DIO</s>ジーク・イェーガー"},
+			{"name":"赫里斯塔·兰斯 | クリスタ・レンズ | 希斯特莉亚·雷斯 | ヒストリア・レイス"},
+		],
+		"review":''
+	},
+	{
+		"time":"2019Spring",
+		"name":"鬼灭之刃 | 鬼滅の刃",
+		"producer":"ufotable",
+		"site":"https://kimetsu.com/anime/risshihen/",
+		"charactor":
+		[
+			{"name":"竈門（かまど） 炭治郎（たんじろう） "},
+			{"name":"竈門（かまど） 禰豆子（ねずこ） "},
+			{"name":"我妻（あがつま） 善逸（ぜんいつ） "},
+			{"name":"嘴平（はしびら） 伊之助嘴平（いのすけ） "},
+			{"name":"栗花落（つゆり） カナヲ"},
+			{"name":"冨岡（とみおか） 義勇（ぎゆう"},
+			{"name":"胡蝶（こちょう） しのぶ "},
+			{"name":"鬼舞辻（きぶつじ） 無惨（むざん）"},
+		],
+		"review":'画风好评。小nezuko好可爱'
+	},
+	{
+		"time":"2019Spring",
+		"name":"贤者之孙 | 賢者の孫",
+		"producer":"SILVER LINK.",
+		"site":"http://kenja-no-mago.jp/",
+		"charactor":
+		[
+			{"name":"西恩·沃夫特（シン＝フォルフォード）"},
+			{"name":"西希莉·冯·克洛德（シシリ＝フォン＝クロード）"},
+			{"name":"玛丽亚·冯·墨西拿（マリア＝フォン＝メッシーナ）"},
+		],
+		"review":'氢弹我是没想到的，不愧是爽文 '
+	},
+	{
+		"time":"2019Winter",
+		"name":"约定的梦幻岛 | 約束のネバーランド",
+		"producer":"CloverWorks",
+		"site":"https://neverland-anime.com/",
+		"charactor":
+		[
+			{"name":"艾玛（エマ）"},
+			{"name":"诺曼（ノーマン）"},
+			{"name":"雷（レイ）"},
+			{"name":"妈妈·伊莎贝拉（マム·イザベラ | ママ）"},
+		],
+		"review":'反转好！坐等1月更新 '
+	},
+	{
+		"time":"2019Winter",
+		"name":"灵能百分百 | モブサイコ100",
+		"producer":"BONES	",
+		"site":"http://mobpsycho100.com/",
+		"charactor":
+		[
+			{"name":"影山（かげやま）茂夫（しげお） | モブ（Mob） "},
+			{"name":"小酒窝（Ekubo）"},
+			{"name":"花泽辉气（Hanazawa Teruki）"},
+		],
+		"review":'骨头社tql，ONEtql，mob yyds '
+	},
+	{
+		"time":"2019Winter",
+		"name":"DATE A LIVE Ⅲ | デート・ア・ライブⅢ",
+		"producer":"J.C.STAFF",
+		"site":"http://date-a-live-anime.com/",
+		"charactor":
+		[
+			{"name":"夜刀神（やとがみ） 十香（とおか）"},
+			{"name":"鳶一（とびいち） 折紙（おりがみ）"},
+			{"name":"時崎（ときさき） 狂三（くるみ）	"},
+			{"name":"氷芽川（ひめかわ） 四糸乃（よしの）"},
+			{"name":"五河（いつか） 琴里（ことり）"},
+			{"name":"鏡野（きょうの） 七罪（なつみ）"},
+			{"name":"八舞（やまい） 耶倶矢（かぐや）"},
+			{"name":"八舞（やまい） 夕弦（ゆづる）"},
+			{"name":"誘宵（いざよい） 美九（みく）"},
+			{"name":"崇宫澪"},
+		],
+		"review":'节操社崩的真是跟kuso一样'
+	},
+	{
+		"time":"2019Winter",
+		"name":"辉夜大小姐想让我告白～天才们的恋爱头脑战～ | かぐや様は告らせたい～天才たちの恋爱头脳戦～",
+		"producer":"A-1 Pictures",
+		"site":"http://kaguya.love/",
+		"charactor":
+		[
+			{"name":"四宮かぐや | Shinomiya Kaguya"},
+			{"name":"白銀御行 | Shirogane Miyuki"},
+			{"name":"藤原千花 | Fujiwara Chika"},
+			{"name":"石上優 | Ishigami Yu"},
+			{"name":"白銀圭 | Shirogane Kei"},
+			{"name":"早坂愛 | Hayasaka Ai"},
+		],
+		"review":'俺没摸妹子，好'
+	},
+	{
+		"time":"2019Winter",
+		"name":"五等分的新娘 | 五等分の花嫁",
+		"producer":"手冢Production",
+		"site":"http://www.tbs.co.jp/anime/5hanayome/",
+		"charactor":
+		[
+			{"name":"上杉（うえすぎ） 風太郎（ふうたろう）"},
+			{"name":"中野（なかの） 一花（いちか）"},
+			{"name":"中野（なかの） 二乃（にの）"},
+			{"name":"中野（なかの） 三玖（みく）"},
+			{"name":"中野（なかの） 四葉（よつば）"},
+			{"name":"中野（なかの） 五月（いつき）"},
+			{"name":"上杉（うえすぎ） らいは  （imouto） "},
+		],
+		"review":'三玖天下第一 '
+	},
+	{
+		"time":"2019Winter",
+		"name":"盾之勇者成名录 | 盾の勇者の成り上がり",
+		"producer":"KINEMA CITRUS",
+		"site":"http://shieldhero-anime.jp/",
+		"charactor":
+		[
+			{"name":"岩谷（いわたに） 尚文（なおふみ）"},
+			{"name":"ラフタリア"},
+			{"name":"フィーロ "},
+		],
+		"review":'          <p class="review"><b id="Filo">FILO大好き！！ '
+	},
+	{
+		"time":"2019Winter",
+		"name":"多罗罗 | どろろ ",
+		"producer":"MAPPA | 手冢Production",
+		"site":"https://dororo-anime.com/",
+		"charactor":
+		[
+			{"name":"百鬼丸 | ひゃっきまる "},
+			{"name":"多罗罗 | どろろ"},
+		],
+		"review":'百鬼丸加入<i>老婆名单</i>，MAPPA社好 '
+	},
+	{
+		"time":"2019Winter",
+		"name":"天使降临到我身边! | 私に天使が舞い降りた！",
+		"producer":"动画工房",
+		"site":"http://watatentv.com/",
+		"charactor":
+		[
+			{"name":"星野（ほしの） みやこ"},
+			{"name":"白咲（しろさき）花（はな） "},
+			{"name":"星野（ほしの）ひなた"},
+			{"name":"姫坂（ひめさか）乃愛（のあ）"},
+			{"name":"種村（たねむら）小依（こより）"},
+			{"name":"小之森（このもり）夏音（かのん）"},
+			{"name":"松本（まつもと）香子（こうこ） "},
+		],
+		"review":'狱友们好！<dd style="color:gold">分かんないけど、みゃ姉が一番好き！<br>虽然不知道，但是我最喜欢宫姐了！</dd> '
+	},
+	{
+		"time":"2018Autumn",
+		"name":"刀剑神域 Alicization | ソードアート・オンライン",
+		"producer":"A-1 Pictures",
+		"site":"https://sao-alicization.net/",
+		"charactor":
+		[
+			{"name":"桐谷和人 | Kirito"},
+			{"name":"Alice | アリス・シンセシス・サーティ"},
+			{"name":"UGO | ユージオ"},
+		],
+		"review":'刀剑好啊（懒得写）'
+	},
+]
